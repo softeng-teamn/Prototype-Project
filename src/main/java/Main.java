2b30dd0300b4ca3766117b99ffd39eb045289dc3
samgoldman <sgoldman216@gmail.com>
2019-03-25 16:42:41 -0400

    Fix path issue
    Cannot start with ./ (jar will fail)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("./home.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("home.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.setTitle("Data View");
